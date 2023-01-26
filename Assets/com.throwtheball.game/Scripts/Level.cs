f990c353f380339d6447ed4890f9b53d8aeef41b
SGCEntertainment <nikcxprojects@gmail.com>
2023-01-26 12:23:33 +0300

    update

--- Assets/com.throwtheball.game/Scripts/Level.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Level : MonoBehaviour
-{
-    private void Start()
-    {
-        transform.position = new Vector2(0, FindObjectOfType<UIManager>().topBorder.position.y - 1.95f);
-    }
-}
