d41cdff6d80a5ed0139986c807d4eacb9d32d41b
SGCEntertainment <nikcxprojects@gmail.com>
2023-01-26 13:29:46 +0300

    update

--- Assets/com.throwtheball.game/Scripts/Managers/SettingsManager.cs
+++ Assets/com.throwtheball.game/Scripts/Managers/SettingsManager.cs
@@ -6,13 +6,25 @@ public class SettingsManager : MonoBehaviour
     [Space(10)]
     [SerializeField] AudioSource loop;
     [SerializeField] Button soundBtn;
+    [SerializeField] Image loopIconImg;
+    [SerializeField] Sprite loopOn;
+    [SerializeField] Sprite loopOff;
 
     [Space(10)]
     [SerializeField] AudioSource sfx;
     [SerializeField] Button sfxBtn;
+    [SerializeField] Image sfxIconImg;
+    [SerializeField] Sprite sfxOn;
+    [SerializeField] Sprite sfxOff;
 
     [Space(10)]
     [SerializeField] Button vibroBtn;
+    [SerializeField] Image vibroIconImg;
+    [SerializeField] Sprite vibroOn;
+    [SerializeField] Sprite vibroOff;
+
+    [Space(10)]
+    [SerializeField] GameObject optionsPopup;
 
     public static bool VibraEnable { get; set; } = false;
 
@@ -24,8 +36,10 @@ public class SettingsManager : MonoBehaviour
         {
             loop.mute = !loop.mute;
 
-            string status = loop.mute ? "OFF" : "ON";
+            string status = loop.mute ? "Music Off" : "Music On";
             soundBtn.transform.GetChild(0).GetComponent<Text>().text = $"{status}";
+
+            loopIconImg.sprite = loop.mute ? loopOff : loopOn;
         });
 
 
@@ -33,20 +47,29 @@ public class SettingsManager : MonoBehaviour
         {
             sfx.mute = !sfx.mute;
 
-            string status = sfx.mute ? "OFF" : "ON";
+            string status = sfx.mute ? "Sounds Off" : "Sounds On";
             sfxBtn.transform.GetChild(0).GetComponent<Text>().text = $"{status}";
+
+            sfxIconImg.sprite = loop.mute ? sfxOff : sfxOn;
         });
 
         vibroBtn.onClick.AddListener(() =>
         {
             VibraEnable = !VibraEnable;
 
-            string status = VibraEnable ? "ON" : "OFF";
+            string status = VibraEnable ? "Vibrations On" : "Vibrations Off";
             vibroBtn.transform.GetChild(0).GetComponent<Text>().text = $"{status}";
+
+            sfxIconImg.sprite = VibraEnable ? vibroOn : vibroOff;
         });
 
         soundBtn.onClick.Invoke();
         sfxBtn.onClick.Invoke();
         vibroBtn.onClick.Invoke();
+    }
+
+    public void OpenOptions(bool IsOpen)
+    {
+        optionsPopup.SetActive(IsOpen);
     }
 }
